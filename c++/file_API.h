5196614c657f051d0e9a76578c17f1427b365108
jordyAjanohoun <jordy.ajanohoun@hotmail.fr>
2018-12-19 17:05:56 +0100

    Makefile + commentaires dans file_API.h

--- c++/file_API.h
+++ c++/file_API.h
@@ -1,4 +1,7 @@
 #include <iostream>
 #include <sstream>
 
+// retourne la premiere matrice lu dans ss_file_content a partir de la position courante du curseur dans la stringstream
+// la taille de la matrice est inscrite dans n
+// retourne la matrice sous forme de tableau 1D alloue dynamiquement donc ne pas oublier de liberer la memoire apres utilisation  
 double * getMatrixFromFile(std::stringstream &ss_file_content, int &n);
