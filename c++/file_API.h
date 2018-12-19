604ae79d9a8028c5c5928edfbac87a559ceac0a9
jordyAjanohoun <jordy.ajanohoun@hotmail.fr>
2018-12-19 17:14:10 +0100

    commentaires ajoutes dans file_API.h

--- c++/file_API.h
+++ c++/file_API.h
@@ -3,5 +3,6 @@
 
 // retourne la premiere matrice lu dans ss_file_content a partir de la position courante du curseur dans la stringstream
 // la taille de la matrice est inscrite dans n
-// retourne la matrice sous forme de tableau 1D alloue dynamiquement donc ne pas oublier de liberer la memoire apres utilisation  
+// retourne la matrice sous forme de tableau 1D alloue dynamiquement donc ne pas oublier de liberer la memoire apres utilisation
+// le format des matrices dans la stringstream doit suivre le format specifie dans le fichier README.md  
 double * getMatrixFromFile(std::stringstream &ss_file_content, int &n);
